d9b89ae33b4bedd4b1a6630dfee0e318850e540b
Ashik <ashikurashik20999@gmail.com>
2026-06-25 13:31:24 +0600

    reverse a string

---
+++ _3_reverse_string.js
@@ -0,0 +1,11 @@
+const isReverseString = (str) =>{
+    let revStr = '';
+
+    for(let i= str.length -1; i>=0; i--){
+        revStr += str[i];
+    }
+
+    return revStr;
+}
+
+console.log(isReverseString("algorithm"));
