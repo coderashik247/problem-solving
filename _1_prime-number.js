127f030a4af804faeb13e2c9db3916951db4043a
Ashik <ashikurashik20999@gmail.com>
2026-06-25 13:04:17 +0600

    prime-number-check add

---
+++ _1_prime-number.js
@@ -0,0 +1,13 @@
+const isPrime = (num) => {
+    if (num <= 1) {
+        return "Not a Prime Number";
+    }
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            return "Not a Prime Number";
+        }
+    }
+    return "Prime Number";
+};
+
+console.log(isPrime(29));
